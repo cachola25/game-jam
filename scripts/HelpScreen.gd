5183e1f42ff36cd321af525cde8d8960ba57444f
moreym26 <moreym26@up.edu>
2024-11-01 19:32:18 -0700

    Added extra details to scenes and connected them together more

--- scripts/HelpScreen.gd
+++ scripts/HelpScreen.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_return_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+	
+	
--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -17,12 +17,10 @@ func _on_help_button_pressed() -> void:
 	pass # Replace with function body.
 
 
-
-
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
 
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
