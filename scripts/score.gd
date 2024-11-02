2c93931febe17d7d5153bfc1993fe7526b4e27dc
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2024-11-02 15:52:51 -0700

    Some final score changes

--- scripts/score.gd
+++ scripts/score.gd
@@ -25,11 +25,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not out_of_time and deincrement_score:
 		score -= 1
-		$Label.text = str(score)
+		$Label.text = "Score: " + str(score)
 		deincrement_score = false
 	if is_talking:
 		is_talking = false
 		score += 30
+	if score == 0:
+		out_of_time = true
+		get_tree().change_scene_to_file("res://scenes/EndScreen.tscn")
 
 func _on_deincrement_timer_timeout() -> void:
 	deincrement_score = true
