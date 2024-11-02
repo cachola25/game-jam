fb2e4751382c63dff4c086c9084388b14008a2e1
moreym26 <moreym26@up.edu>
2024-11-02 10:41:17 -0700

    Made audio play in select scenes
    Made good audio play continuously thru help and start screen, made chatter play in background of main scene.

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -3,10 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-
+	TitleScreenMusic._playMusicLevel()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,4 +21,5 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_start_button_pressed() -> void:
+	TitleScreenMusic._stopMusicLevel()
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
