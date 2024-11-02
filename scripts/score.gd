0d3e2d028aef90f66a88dc14f8e54d5da3c28988
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2024-11-01 21:28:55 -0700

    modified scoring system

--- scripts/score.gd
+++ scripts/score.gd
@@ -2,24 +2,23 @@ extends Node2D
 
 @onready var score
 @onready var is_talking
-@onready var timer
 @onready var out_of_time
 @onready var deincrement_score
 @onready var ate
 @onready var drank
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	score = 100
+	score = 60
 	deincrement_score = false
 	is_talking = true
 	out_of_time = false
-	timer = $Timer
 	ate = false
 	drank = false
 	$DeincrementTimer.start()
 func _process(delta: float) -> void:
 	if is_talking and not out_of_time and deincrement_score:
 		score -= 1
+		$Label.text = str(score)
 		print(score)
 		deincrement_score = false
 	if ate or drank:
