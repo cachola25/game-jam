0cf5dfff6801ae1dc56fc0301f17a4c5ab5b3268
kayv1273 <vuk25@up.edu>
2024-11-02 17:53:56 -0700

    Fixed win buttons

--- scripts/WinningScore.gd
+++ scripts/WinningScore.gd
@@ -9,3 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/TitleScreen.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
